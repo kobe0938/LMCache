f6adad3012e74ad8aeedaf51a75289f81a50d520
Xiaokun <xiaokunchen0@gmail.com>
2024-12-08 11:10:29 +0900

    docekr config

--- router/log_request_server/main.py
+++ router/log_request_server/main.py
@@ -73,5 +73,5 @@ async def chat_completions(data: dict):
     return {"status": "completed", "data_received": data}
 
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8080)
+# if __name__ == "__main__":
+#     uvicorn.run(app, host="0.0.0.0", port=8080)
